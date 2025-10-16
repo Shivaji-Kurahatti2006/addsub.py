8eade0a3a28b923e7f2ef766c5f479d51cecf0c4
SHIVAJI KURAHATTI <01fe24bca241@kletech>
2025-10-16 20:44:39 +0530

    MY  fourth file

---
+++ addsub.py
@@ -0,0 +1,15 @@
+# Simple Python code to perform addition and subtraction
+
+# Input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Addition
+add_result = num1 + num2
+
+# Subtraction
+sub_result = num1 - num2
+
+# Output the results
+print("Addition result:", add_result)
+print("Subtraction result:", sub_result)
